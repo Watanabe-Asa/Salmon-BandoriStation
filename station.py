309dfe0d2bac6ce0f43774bd37016b98497a012e
しゃけてり <65025615+Watanabe-Asa@users.noreply.github.com>
2021-01-12 03:11:08 +0800

    fix bug

--- station.py
+++ station.py
@@ -1,18 +1,41 @@
 import requests
 import json
 import asyncio
+import copy
 import nonebot
 import hoshino
-from hoshino import Service
+from os import path
+from hoshino import Service, priv
 from datetime import datetime
 
 sv1 = Service('车站查询', enable_on_default=True)
 sv2 = Service('车站推送', enable_on_default=False)
 
+config = {}
+config_path = path.join(path.dirname(__file__), "config.json")
+with open(config_path, "r", encoding="utf8")as fp:
+    config = json.load(fp)
+
+
+def load_config():
+    global config
+    config_path = path.join(path.dirname(__file__), "config.json")
+    with open(config_path, "r", encoding="utf8")as fp:
+        config = json.load(fp)
+
+
+def save_config():
+    config_path = path.join(path.dirname(__file__), "config.json")
+    jsonStr = json.dumps(config, indent=4)
+    with open(config_path, "r+", encoding="utf8")as fp:
+        fp.truncate(0)
+        fp.seek(0)
+        fp.write(jsonStr)   
+
 
 @sv1.on_suffix('车站人数')
 async def query_number(bot, ev):
-    resp = json.loads(requests.get('https://api.bandoristation.com/?function=get_online_number').content.decode('utf-8'))
+    resp = json.loads(requests.get('https://api.bandoristation.com/?function=get_online_number').content.decode('utf8'))
     status = resp['status']
     if status == 'failure':
         sv1.logger.info('Api出错，请停用插件')
@@ -25,7 +48,7 @@ async def query_number(bot, ev):
 
 @sv1.on_fullmatch(('查询房间', '查看房间', '房间查询'))
 async def query_room(bot, ev):
-    resp = json.loads(requests.get('https://api.bandoristation.com/?function=query_room_number').content.decode('utf-8'))
+    resp = json.loads(requests.get('https://api.bandoristation.com/?function=query_room_number').content.decode('utf8'))
     status = resp['status']
     if status == 'failure':
         sv1.logger.info('Api出错，请停用插件')
@@ -67,11 +90,37 @@ async def query_room(bot, ev):
             await bot.send(ev, result)
 
 
-@sv2.scheduled_job('interval', minutes=2)
+@sv2.on_prefix('开启通知')
+async def add_group(bot, ev):
+    if not priv.check_priv(ev, priv.ADMIN):
+        await bot.finish(ev, '请联系群管理为本群开启车站通知.', at_sender=True)
+    gid = str(ev.group_id)
+    if not gid in config['station_notice']:
+        config['station_notice'][gid] = {"gid":gid, "notice_on":True}
+    else:
+        await bot.finish(ev, '本群已经开启车站通知了.', at_sender=True)
+    save_config()
+    await bot.send(ev, '开启成功', at_sender=True)
+
+
+@sv2.on_prefix('关闭通知')
+async def delete_group(bot, ev):
+    if not priv.check_priv(ev, priv.ADMIN):
+        await bot.finish(ev, '请联系群管理为本群关闭车站通知.', at_sender=True)
+    gid = str(ev.group_id)
+    if not gid in config['station_notice']:
+        await bot.finish(ev, '本群还没有开启车站通知.', at_sender=True)
+    else:
+        config['station_notice'][gid]['notice_on'] = False
+        save_config()
+        await bot.send(ev, '关闭成功', at_sender=True)
+
+
+@sv2.scheduled_job('interval', minutes=1)
 async def query_schedule():
     bot = nonebot.get_bot()
     weihu = hoshino.config.SUPERUSERS[0]
-    resp = json.loads(requests.get('https://api.bandoristation.com/?function=query_room_number').content.decode('utf-8'))
+    resp = json.loads(requests.get('https://api.bandoristation.com/?function=query_room_number').content.decode('utf8'))
     status = resp['status']
     if status == 'failure':
         sv2.logger.info('Api出错，请停用插件')
@@ -110,10 +159,11 @@ async def query_schedule():
                 putline.append('发布用户：{}({})'.format(username, user_id))
                 putline.append('发布来源：{}({})'.format(name, source_type))
                 putline.append('房间说明：{}'.format(raw_message))
-            result = "\n".join(putline)
-            for sid in hoshino.get_self_ids():
-                gl = await bot.get_group_list(self_id=sid)
-                gl = [ g['group_id'] for g in gl ]
-                for g in gl:
-                    await asyncio.sleep(0.5)
-                    await bot.send_group_msg(self_id=sid, group_id=g, message=result)
+            msg = "\n".join(putline)
+            station_notice = copy.deepcopy(config['station_notice'])
+            for gid in station_notice:
+                gid = str(gid)
+                await asyncio.sleep(1.5)
+                if config['station_notice'][gid]['notice_on']:
+                    await bot.send_group_msg(group_id=int(config['station_notice'][gid]['gid']), message=msg)
+                    await asyncio.sleep(1.5)
